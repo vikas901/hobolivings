c44260751eba9dc6b5ccda173a0e045f85572733
Vikash Sagar <sagarvikas901@gmail.com>
2025-07-30 20:04:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/property-listing-form.tsx
+++ src/components/property-listing-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { zodResolver } from '@hookform/resolvers';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, useFieldArray } from 'react-hook-form';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
