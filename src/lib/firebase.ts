6f16eac09dddd25dc496648cf2ef7e814993bacc
Vikash Sagar <sagarvikas901@gmail.com>
2025-07-31 06:41:42 +0000

    [2025-07-31T06:33:40.913Z]  @firebase/firestore: Firestore (10.14.1): en

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { initializeFirestore, persistentLocalCache } from 'firebase/firestore';
+import { initializeFirestore, persistentLocalCache, memoryLocalCache, CACHE_SIZE_UNLIMITED } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
 
 const firebaseConfig = {
   projectId: 'hobo-livings',
@@ -16,10 +18,13 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 
 // Initialize Firestore with persistent caching
 const db = initializeFirestore(app, {
-  cache: persistentLocalCache({}),
+  localCache: persistentLocalCache({
+    cacheSizeBytes: CACHE_SIZE_UNLIMITED,
+  }),
 });
 
 
